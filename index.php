bfbadbd1b4c2da96f4112e9ae6fc73dc5e3a76c3
nhatminh25 <haminh0205@gmail.com>
2021-10-27 10:35:37 +0700

    title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Minh béo</title>
+    <title>Trang Chủ</title>
   </head>
   <body>
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
