1f36a4c108c413187021a1f657b02ecd43d63415
gianam65 <gianam65012001@gmail.com>
2021-10-28 10:10:49 +0700

    Add process and error page

--- route/admin/index.php
+++ route/admin/index.php
@@ -11,53 +11,58 @@
   <body>
     <div class="container">
         <h3 class="global-title">Admin book manager</h3>
-        <a href="../.././src/process-add.php" class="btn btn-success add-car-btn">Add book</a>
-        <table class="table table-info list-book">
-            <thead>
-                <tr>
-                    <th scope="col">STT</th>
-                    <th scope="col">Tên sách</th>
-                    <th scope="col">Giá sách</th>
-                    <th scope="col">Tên tác giả</th>
-                    <th scope="col">Trạng thái</th>
-                    <th scope="col">Ảnh</th>
-                    <th scope="col">Hành động</th>
-                    <th scope="col">Hành động</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $conn = mysqli_connect('localhost','root','','database_book');
-                    if(!$conn) {
-                        die("Không thể kết nối");
-                    }
-                    $sql = "SELECT book_id,book_name,book_price, book_author, book_status, book_image FROM tb_books";
-                    $result = mysqli_query($conn,$sql);
-                    
-                    if(mysqli_num_rows($result) > 0){
-                        while($row = mysqli_fetch_assoc($result)) {
-                            if($row["book_status"] == 0) {
-                                $row["book_status"] = "Đã cung cấp ";
-                            } else {
-                                $row["book_status"] = "Chưa cung cấp";
-                            }
-                            echo "<tr>";
-                                echo "<th>".$row["book_id"]."</th>";
-                                echo "<th>".$row["book_name"]."</th>";
-                                echo "<th>".$row["book_price"]."</th>";
-                                echo "<th>".$row["book_author"]."</th>";
-                                echo "<th>".$row["book_status"]."</th>";
-                                echo "<th>
-                                    <img class=dashboard-book-img src=$row[book_image] />
-                                </th>";
-                                echo "<th><a href=../.././src/process-delete.php?id=".$row["book_id"]." class='btn btn-warning'>Delete</a></th>";
-                                echo "<th><a href=src/update.php?idUpdate=".$row["book_id"]." class='btn btn-secondary'>Update</a></th>";
-                            echo "</tr>";
+        <a href="addbook.php" class="btn btn-success add-book-btn">Add book</a>
+        <div class="book-container">
+            <table class="table table-info list-book">
+                <thead>
+                    <tr>
+                        <th scope="col">STT</th>
+                        <th scope="col">Tên sách</th>
+                        <th scope="col">Giá sách</th>
+                        <th scope="col">Tên tác giả</th>
+                        <th scope="col">Trạng thái</th>
+                        <th scope="col">Ảnh</th>
+                        <th scope="col">Hành động</th>
+                        <th scope="col">Hành động</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $conn = mysqli_connect('localhost','root','','database_book');
+                        if(!$conn) {
+                            die("Không thể kết nối");
                         }
-                    }
-                ?>
-            </tbody>
-        </table>
+                        $sql = "SELECT book_id,book_name,book_price, book_author, book_status, book_image FROM tb_books";
+                        $result = mysqli_query($conn,$sql);
+                        
+                        if(mysqli_num_rows($result) > 0){
+                            $index = 0;
+                            while($row = mysqli_fetch_assoc($result)) {
+                                $index++;
+                                if($row["book_status"] == 0) {
+                                    $row["book_status"] = "Chưa cung cấp ";
+                                } else {
+                                    $row["book_status"] = "Đã cung cấp";
+                                }
+                                
+                                echo "<tr>";
+                                    echo "<th>".$index."</th>";
+                                    echo "<th>".$row["book_name"]."</th>";
+                                    echo "<th>".$row["book_price"]."</th>";
+                                    echo "<th>".$row["book_author"]."</th>";
+                                    echo "<th>".$row["book_status"]."</th>";
+                                    echo "<th>
+                                        <img class=dashboard-book-img src=$row[book_image] />
+                                    </th>";
+                                    echo "<th><a href=../.././src/process-delete.php?id=".$row["book_id"]." class='btn btn-warning'>Delete</a></th>";
+                                    echo "<th><a href=src/update.php?idUpdate=".$row["book_id"]." class='btn btn-secondary'>Update</a></th>";
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
---
+++ src/process-add.php
@@ -0,0 +1,25 @@
+<?php
+    if(isset($_POST['addBook'])) {
+        $bookName = $_POST['bookName'];
+        $bookPrice = $_POST['bookPrice'];
+        $bookAuthor = $_POST['bookAuthor'];
+        $bookDesc = $_POST['bookDesc'];
+        $bookImage = $_POST['bookImage'];
+        $bookStatus = $_POST['bookStatus'];
+        if($bookImage == '' || $bookImage == null ||  !$bookImage) {
+            $bookImage == '../.././images/notfound.png';
+        }
+        $conn = mysqli_connect('localhost','root','','database_book');
+        if(!$conn) {
+            die("Không thể kết nối");
+        }
+        $sql = "INSERT INTO tb_books(book_name, book_price, book_author, book_desc, book_image, book_status) VALUES ('$bookName','$bookPrice','$bookAuthor','$bookDesc','$bookImage','$bookStatus')";
+        
+        $result = mysqli_query($conn,$sql);
+        if($result > 0){
+            header("Location: .././route/admin/index.php");
+        }else{
+            header("Location: .././error.php");
+        }
+    }
+?>
