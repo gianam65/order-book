8bf6ebae744e2eb69b1c6d0d646682e323f3b44f
nhatminh25 <haminh0205@gmail.com>
2021-10-28 18:48:49 +0700

    improve error page

--- error.php
+++ error.php
@@ -1,11 +1,12 @@
 <link rel="stylesheet" href="./css/error.css">
 <h1>404 Error Page</h1>
-<p class="zoom-area"><b>Thông tin bạn cần tìm không tồn tại</b></p>
+    <p class="zoom-area"><b>Thông tin bạn cần tìm không tồn tại</b></p>
 <section class="error-container">
-  <span class="four"><span class="screen-reader-text">4</span></span>
-  <span class="zero"><span class="screen-reader-text">0</span></span>
-  <span class="four"><span class="screen-reader-text">4</span></span>
+    <span class="four"><span class="screen-reader-text">4</span></span>
+    <span class="zero"><span class="screen-reader-text">0</span></span>
+    <span class="four"><span class="screen-reader-text">4</span></span>
 </section>
+
 <div class="link-container">
-  <a target="_blank" href="http://localhost:8080/order-book/index.php" class="more-link">Quay về trang chủ</a>
+    <a target="_blank" href="http://localhost:8080/order-book/index.php" class="more-link">Quay về trang chủ</a>
 </div>
