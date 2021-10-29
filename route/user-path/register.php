a528615ed21af86a35f9cef81455acf5f3d64020
Nghia241 <nghap1@gmail.com>
2021-10-29 21:20:37 +0700

    login header

--- route/user-path/register.php
+++ route/user-path/register.php
@@ -59,7 +59,7 @@
 				<td><input type="text" id="email" name="email"></td>
 			</tr>
 			<tr>
-				<td colspan="2" align="center"><input type="submit" name="btn_submit" value="Dang ky"></td>
+				<td colspan="2" align="center"><input type="submit" name="btn_submit" value="Đăng ký"></td>
 			</tr>
 
 		</table>
