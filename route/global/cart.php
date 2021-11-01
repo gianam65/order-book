30968770c75a0745955a350068661fa254c87bc0
nhatminh25 <haminh0205@gmail.com>
2021-11-01 11:28:38 +0700

    navbar cart book

--- route/global/cart.php
+++ route/global/cart.php
@@ -4,14 +4,63 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Giỏ hàng</title>
+    <title>Giỏ Hàng</title>
     <link rel="stylesheet" href="./css/style.css">                                 
 </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light home-nav">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse home-menu" id="navbarTogglerDemo01">
+                    <a class="navbar-brand home-link" href="#">TRANG CHỦ</a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link home-link" href="#"><span class="sr-only">Đăng kí</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link home-link" href="#">Đăng nhập</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
+        <div class="container">
+            <h2 class="text-center">Giỏ Hàng</h2>
+            <div class="span8">
+                <div class="box-list-card">
+                    <div class="list-book">
+                        <div>
+                            <div class="mainbox-card-body">
+                                <div id="card-items">
+                                    <div class="table-card-items">
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th colspan="3">Sản Phẩm</th>
+                                                </tr>
+                                            </thead>
 
-
-    
+                                            <tbody class="updown_wrapper">
+                                                <tr>
+                                                    <td width="8%">
+                                                        <div class="thumb-book">
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
