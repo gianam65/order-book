d9ed7900bbaa1500c270dd20aeb3a9e55ff1af20
Nghia241 <90234363+Nghia241@users.noreply.github.com>
2021-11-01 10:11:17 +0700

    improve login and register

--- route/global/login.php
+++ route/global/login.php
@@ -1,101 +1,28 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Đăng nhập</title>
-    <link rel="stylesheet" href="../.././css/admin-dashboard.css">
-    <link rel="stylesheet" href="../.././css/style.css">
-  </head>
-  <body>
-  	<nav class="navbar navbar-expand-lg navbar-light bg-light home-nav">
-		<div class="container">
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse home-menu" id="navbarTogglerDemo01">
-				<a class="navbar-brand" href="#">TRANG CHỦ</a>
-				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-					<li class="nav-item active">
-						<a class="nav-link" href="#"><span class="sr-only">ĐĂNG KÍ</span></a>
-					</li>
-
-					<li class="nav-item">
-						<a class="nav-link" href="#">ĐĂNG NHẬP</a>
-					</li>
-				</ul>
-
-				<form class="form-inline my-2 my-lg-0 search-section">
-					<input class="form-control mr-sm-2" type="search" placeholder="Nhập tên sách" aria-label="search">
-					<button class="btn btn-outline-success my-2 my-sm-0 search-btn" type="submit">
-						TÌM KIẾM
-					</button>
-				</form>
-			</div>
-		</div>
-	</nav>
-  <?php
-	session_start();
-?>
-
-	<?php 
-if (isset($_POST["btn_submit"])) {
-	// lấy thông tin người dùng
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	//làm sạch thông tin, xóa bỏ các tag html, ký tự đặc biệt 
-	//mà người dùng cố tình thêm vào để tấn công theo phương thức sql injection
-	$username = strip_tags($username);
-	$username = addslashes($username);
-	$password = strip_tags($password);
-	$password = addslashes($password);
-	if ($username == "" || $password =="") {
-		echo "username hoặc password bạn không được để trống!";
-	}else{
-		$conn = mysqli_connect('localhost','root','','database_book');
-        if(!$conn){
-            die('Không thể kết nối');
-        }
-		$sql = "select * from tb_users where user_name = '$username' and user_pass = '$password' ";
-		$query = mysqli_query($conn,$sql);
-		$num_rows = mysqli_num_rows($query);
-		if ($num_rows==0) {
-			echo "tên đăng nhập hoặc mật khẩu không đúng !";
-		}else{
-			// Lấy ra thông tin người dùng và lưu vào session
-			while ( $data = mysqli_fetch_array($query) ) {
-	    		$_SESSION["user_id"] = $data["id"];
-				$_SESSION['username'] = $data["username"];
-				$_SESSION["email"] = $data["email"];
-				$_SESSION["fullname"] = $data["fullname"];
-				$_SESSION["is_block"] = $data["is_block"];
-				$_SESSION["permision"] = $data["permision"];
-	    	}
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../.././css/login.css">
+	<title>Đăng nhập</title>
+</head>
+<body>
+	<div class="container">
+		<form action="../.././src/process-login.php" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Đăng nhập</p>
 			
-                // Thực thi hành động sau khi lưu thông tin vào session
-                // ở đây mình tiến hành chuyển hướng trang web tới một trang gọi là index.php
-			header('Location: ../.././index.php');
-		}
-	}
-}
-?>
-	<form method="POST" action="login.php">
-	<fieldset>
-	    <legend>Đăng nhập</legend>
-	    	<table>
-	    		<tr>
-	    			<td>Username</td>
-	    			<td><input type="text" name="username" size="30"></td>
-	    		</tr>
-	    		<tr>
-	    			<td>Password</td>
-	    			<td><input type="password" name="password" size="30"></td>
-	    		</tr>
-	    		<tr>
-	    			<td colspan="2" align="center"> <input type="submit" name="btn_submit" value="Đăng nhập"></td>
-	    		</tr>
-	    	</table>
-  </fieldset>
-  </form>
+			<div class="input-group">
+				<input type="text" placeholder="Tên đăng nhập" name="username">
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Mật khẩu" name="password">
+			</div>
+			<div class="input-group">
+				<button name="login" class="btn">Đăng nhập</button>
+			</div>
+			<p class="login-register-text">Chưa có tài khoản ? <a href="./register.php">Đăng ký tại đây</a>.</p>
+		</form>
+	</div>
+</body>
+</html>
