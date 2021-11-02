92fc763ea28a5bf0e94f8a53967db689b7694429
Nghia241 <90234363+Nghia241@users.noreply.github.com>
2021-11-02 09:55:03 +0700

    add validate form

--- src/process-register.php
+++ src/process-register.php
@@ -16,10 +16,17 @@
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result) > 0) {
-        echo("Username đã tồn tại");
+        echo '<script>alert("Tên đăng nhập đã tồn tại")</script>';
+        echo "<script>window.location = '.././route/global/register.php'</script>";
     } else {
         $pass_hash = password_hash($pass1, PASSWORD_DEFAULT);
         $sql2 = "INSERT INTO tb_users(user_name, user_email, user_pass, user_phone) VALUES ('$user', '$user_email','$pass_hash', '$phone')";
+        if (empty($user) || empty($pass1) || empty($pass2) || empty($phone) || empty($user_email)) {
+            $sql2 = "INSERT INTO tb_usersss(user_name, user_email, user_pass, user_phone) VALUES ('$user', '$user_email','$pass_hash', '$phone')";
+			echo '<script>alert("Bạn vui lòng nhập đầy đủ thông tin")</script>';
+            echo "<script>window.location = '.././route/global/register.php'</script>";
+            return; 
+		}
         $result2 = mysqli_query($conn, $sql2);
         if($result2 == true) {
             header("Location: .././index.php");
